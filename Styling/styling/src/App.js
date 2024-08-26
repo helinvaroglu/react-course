2289748f13158085c6610281ca399256f53e7ace
helinvaroglu <helinv@sabanciuniv.edu>
2024-08-26 20:19:40 +0300

    Completed styling part

--- Styling/styling/src/App.js
+++ Styling/styling/src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+// We need to enter styling as an object
+// For every component, create their owm css file and use module css
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div style={{ color: "red", backgroundColor: "white" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna 
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+          ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
       </header>
     </div>
   );
