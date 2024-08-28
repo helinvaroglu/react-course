ee02c8761542dc886870869da93a191c1c906167
helinvaroglu <helinv@sabanciuniv.edu>
2024-08-28 23:03:22 +0300

    Initialized contacts app

---
+++ ContactsApp/contacts_app/src/components/Contacts/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import List from './List/index.js';
+import Form from './Form/index.js';
+
+
+function Contacts() {
+  return (
+    <div>
+        <List />
+        <Form />
+    </div>
+  )
+}
+
+export default Contacts;
