b019903d0301398e0a26b198a6aa545618bcbf2f
helinvaroglu <helinv@sabanciuniv.edu>
2024-08-29 00:46:16 +0300

    Completed list component

--- ContactsApp/contacts_app/src/components/Contacts/List/index.js
+++ ContactsApp/contacts_app/src/components/Contacts/List/index.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import {useState} from 'react'
 
 function List({contacts}) {
-  return (
-    <div>
-        <ul>
-            {contacts.map((contact, i) => (
-                <li key={i}>{contact.fullname}</li>
-            ))}
-        </ul>
-    </div>
-  )
+    const [filterText, setFilterText] = useState('');
+
+    const filtered = contacts.filter((item) => {
+        return Object.keys(item).some((key) => 
+            item[key].toString().toLowerCase().includes(filterText.toLowerCase())
+        );
+    });
+
+    return (
+        <div>
+
+            <input placeholder="Filter contacts" value={filterText} onChange={(e) => setFilterText(e.target.value)}></input>
+            <ul className="list">
+				{filtered.map((contact, i) => (
+					<li key={i}>
+						<span>{contact.fullname}</span>
+						<span>{contact.phone_number}</span>
+					</li>
+				))}
+			</ul>
+
+            <p>Total contacts ({filtered.length})</p>
+        </div>
+    )
 }
 
 export default List;
--- ContactsApp/contacts_app/src/components/Contacts/index.js
+++ ContactsApp/contacts_app/src/components/Contacts/index.js
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react';
 
+
 import List from './List/index.js';
 import Form from './Form/index.js';
 
