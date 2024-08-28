0f3dc7705fc6e4ef209ae7b2270d236b28df1251
helinvaroglu <helinv@sabanciuniv.edu>
2024-08-28 23:46:19 +0300

    Developed form component

--- ContactsApp/contacts_app/src/components/Contacts/Form/index.js
+++ ContactsApp/contacts_app/src/components/Contacts/Form/index.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import {useState} from 'react'
 
 function Form() {
-  return (
-    <div>Form</div>
-  )
+    const [form, setForm] = useState({ fullname: '', phone_number: ''});
+
+    const onChangeInput = (e) => {
+        setForm({...form, [e.target.name]: e.target.value});
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if (form.fullname === "" || form.phone_number === "") {
+            return false;
+        }
+        
+        console.log(form);
+    };
+
+
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div>
+                <div>
+                    <input 
+                        name="fullname" 
+                        placeholder="Full Name"
+                        onChange={onChangeInput}
+                    />
+                </div>
+
+                <div>
+                    <input 
+                        name="phone_number"
+                        placeholder="Phone Number"
+                        onChange={onChangeInput}
+                    />
+                </div>
+
+                <div>
+                    <button>Add</button>
+                </div>
+            </div>
+        </form>
+    )
 }
 
 export default Form;
