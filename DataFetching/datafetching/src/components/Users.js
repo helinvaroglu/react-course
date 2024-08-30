098b767b4597e0308a78217f864bc66f844ec10d
helinvaroglu <helinv@sabanciuniv.edu>
2024-08-30 13:49:06 +0300

    Finished data fetching part

--- DataFetching/datafetching/src/components/Users.js
+++ DataFetching/datafetching/src/components/Users.js
@@ -1,5 +1,11 @@
 import {useEffect} from 'react'
 
+import axios from 'axios';
+
+// axios has wider web support and it returns a jso object
+// more preferable
+// only changing keyword 'fetch' with 'axios'
+
 function Users() {
 
     // component should reach the data when it is mounted
@@ -8,7 +14,7 @@ function Users() {
     // and you need to call .json() or .text() on the Response to extract the data.
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
-            .then((res) => res.json())
+            .then((res) => res.json()) // fetch's res is a body not a json so you need to convert
             .then((users) => console.log(users));
     }, []);
 
