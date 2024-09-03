1fadd830dcf1203caefadac7705a83c5a07defb6
helinvaroglu <helinv@sabanciuniv.edu>
2024-09-03 20:43:43 +0300

    Finished colors app

--- RealtimeApps/ColorsApp/client/colorsapp/src/App.js
+++ RealtimeApps/ColorsApp/client/colorsapp/src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
-import Palette from './components/Palette'
+import "./App.css";
+
+import { useEffect, useState } from "react";
+import { init, subscribe } from "./socketApi";
+import Palette from "./components/Palette";
 
 function App() {
-  return (
-    <div className="App">
-      <Palette />
-    </div>
-  );
+	const [activeColor, setActiveColor] = useState("#282c34");
+
+	useEffect(() => {
+		init();
+
+		subscribe((color) => {
+			setActiveColor(color);
+		});
+	}, []);
+
+	return (
+		<div className="App" style={{ backgroundColor: activeColor }}>
+			<h1>{activeColor}</h1>
+			<Palette activeColor={activeColor} />
+		</div>
+	);
 }
 
 export default App;
